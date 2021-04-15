8321f3d27df6141d8292afc0f11d7b3bb70e2932
Gilles Boccon-Gibod <bok@bok.net>
2021-04-15 16:37:43 -0700

    add library error code mappings

--- xp/tls/gg_tls.h
+++ xp/tls/gg_tls.h
@@ -183,6 +183,7 @@ typedef struct {
 #define GG_ERROR_TLS_UNKNOWN_IDENTITY              (GG_ERROR_BASE_TLS - 1)
 #define GG_ERROR_TLS_BAD_CLIENT_HELLO              (GG_ERROR_BASE_TLS - 2)
 #define GG_ERROR_TLS_BAD_SERVER_HELLO              (GG_ERROR_BASE_TLS - 3)
+#define GG_ERROR_TLS_UNMAPPED_LIB_ERROR            (GG_ERROR_BASE_TLS - 4)
 
 /**
  * Event type emitted by a DTLS protocol object when its state changes.
