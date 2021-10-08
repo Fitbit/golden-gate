41c6bc56609fb3a06e03ff98d888f1a8b3d0f15f
Andy Branscomb <abranscomb@fitbit.com>
2021-10-08 01:06:21 +0000

    Merge pull request #85 in XP/xp-v-golden-gate from andy/FC-6616 to master
    Squashed commit of the following:
    
    commit bddfa86f4f2d21b7e6e64428cdddf1a5503320ed
    Author: Andy Branscomb <abranscomb@google.com>
    Date:   Thu Oct 7 02:31:16 2021 +0000
    
        FC-6616: Prevent emitting response bodies on GG Thread
    
        This fixes the proximate cause of the ANR listed in this ticket.
        The root cause is that the Android bindings do not yet support
        GG_COAP_REQUEST_HANDLER_FLAG_ENABLE_ASYNC and therefore block
        the GG thread when processing incoming requests.

--- platform/android/goldengate/GoldenGateBindings/src/main/kotlin/com/fitbit/goldengate/bindings/coap/handler/BaseRxResourceHandler.kt
+++ platform/android/goldengate/GoldenGateBindings/src/main/kotlin/com/fitbit/goldengate/bindings/coap/handler/BaseRxResourceHandler.kt
@@ -4,6 +4,9 @@
 package com.fitbit.goldengate.bindings.coap.handler
 
 import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.internal.schedulers.TrampolineScheduler
+import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.PublishSubject
 
 
@@ -21,13 +24,34 @@ abstract class BaseRxResourceHandler : BaseResourceHandler() {
      * of the data(POST, PUT request etc.) depends on the implementation of the class that extends
      * [BaseRxResourceHandler]
      *
-     * @return An [Observable] that emits data as a ByteArray
+     * Emissions will be on [Schedulers.computation()]. If you wish to use another scheduler, use
+     * the overload that takes a [Scheduler].
+     *
+     * @return An [Observable] that emits data as a ByteArray on [Schedulers.computation()].
      */
-    fun observeUpdates(): Observable<ByteArray> = updateSubject.hide()
+    fun observeUpdates(): Observable<ByteArray> = observeUpdates(Schedulers.computation())
+
+    /**
+     * Gets an Observable that will emit the data from the body of the incoming requests. The source
+     * of the data(POST, PUT request etc.) depends on the implementation of the class that extends
+     * [BaseRxResourceHandler]
+     *
+     * Emissions will be on [scheduler]. Passing a [TrampolineScheduler] is not allowed.
+     *
+     * @throws IllegalArgumentException if [scheduler] is a [TrampolineScheduler]
+     *
+     * @return An [Observable] that emits data as a ByteArray on [scheduler]
+     */
+    fun observeUpdates(scheduler: Scheduler): Observable<ByteArray> {
+        if (scheduler is TrampolineScheduler) {
+            throw IllegalArgumentException("Not allowed to use a TrampolineScheduler. This could result in blocking the Golden Gate thread.")
+        }
+        return updateSubject.hide().observeOn(scheduler)
+    }
 
     /**
      * Instructs the internal [PublishSubject] to emit data. This method is intended to be used inside
-     * handlers that extend the [BaseRxResourceHandler] when an [onPost] [onPut] is override
+     * handlers that extend the [BaseRxResourceHandler] when an [onPost] [onPut] is overridden
      */
      protected fun emitUpdates(data: ByteArray) = updateSubject.onNext(data)
 }
